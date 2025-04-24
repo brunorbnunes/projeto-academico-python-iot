fb0abb47a36a51a28bb8b7eb69f5b2e410c70146
brunorbnunes <brunolcbjlf@gmail.com>
2025-04-24 18:45:07 -0300

    inserir dados

--- Conn.py
+++ Conn.py
@@ -16,3 +16,30 @@ c.execute('''
     )
 ''')
 conn.commit()
+
+while True:
+    line = ser.readline().decode('utf-8').strip()
+    print(f"Linha lida: {line}")
+
+    if line.startswith("VALID"):
+        try:
+            _, uid, date, time = line.split(',')
+
+            
+            print(f"UID: {uid}, Data: {date}, Hora: {time}")
+
+            
+            c.execute('INSERT INTO registros (uid, data, hora) VALUES (?, ?, ?)', (uid.strip(), date.strip(), time.strip()))
+            conn.commit()
+            print("Dados armazenados no banco de dados.")
+
+            c.execute('SELECT * FROM registros')
+            registros = c.fetchall()
+
+            for registro in registros:
+                print(f"ID: {registro[0]}, UID: {registro[1]}, Data: {registro[2]}, Hora: {registro[3]}")
+
+        except ValueError as e:
+            print(f"Erro ao processar a linha: {line}. Erro: {e}")
+    else:
+        print("Dados inválidos, UID não reconhecido.")
